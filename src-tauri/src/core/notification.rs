c97b9f014e19c45d5aa73344dc2c544c9a158e25
sun_y <sun_y@example.com>
2026-06-22 18:22:45 +0800

    style: 自动格式化 FrontendEvent enum 为多行样式

--- src-tauri/src/core/notification.rs
+++ src-tauri/src/core/notification.rs
@@ -9,12 +9,26 @@ use tauri::{Emitter as _, WebviewWindow};
 pub enum FrontendEvent<'a> {
     RefreshClash,
     RefreshVerge,
-    NoticeMessage { status: &'a str, message: String },
-    ProfileChanged { current_profile_id: &'a String },
-    TimerUpdated { profile_index: &'a String },
-    ProfileUpdateStarted { uid: &'a String },
-    ProfileUpdateCompleted { uid: &'a String },
-    DelayResults { group: std::string::String, results: Vec<(std::string::String, u32)> },
+    NoticeMessage {
+        status: &'a str,
+        message: String,
+    },
+    ProfileChanged {
+        current_profile_id: &'a String,
+    },
+    TimerUpdated {
+        profile_index: &'a String,
+    },
+    ProfileUpdateStarted {
+        uid: &'a String,
+    },
+    ProfileUpdateCompleted {
+        uid: &'a String,
+    },
+    DelayResults {
+        group: std::string::String,
+        results: Vec<(std::string::String, u32)>,
+    },
 }
 
 #[derive(Debug)]
